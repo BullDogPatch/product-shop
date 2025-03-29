428d5408039983cbe76b64a1fe916106602598ad
Craig Clayton <craig__clayton@outlook.com>
2025-03-29 18:03:32 +0000

    change border on bottom of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from './ModeToggle';
 
 const Header = () => {
   return (
-    <header className='h-24 p-4 flex justify-between items-center border border-b-amber-800'>
+    <header className='h-24 p-4 flex justify-between items-center border-b-3 border-b-amber-800 '>
       <p>Shit-Shop</p>
       <ModeToggle />
     </header>
