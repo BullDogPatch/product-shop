102821613521d0f46ebbd66bdfd472a547146736
Craig Clayton <craig__clayton@outlook.com>
2025-03-30 16:23:50 +0100

    install react-icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 import CartTotal from './CartTotal';
 import { ModeToggle } from './ModeToggle';
+import { FaCartShopping } from 'react-icons/fa6';
 
 const Header = () => {
   return (
     <header className='h-24 p-4 flex justify-between items-center border-b-3 border-b-amber-800 '>
       <p>Shit-Shop</p>
-      <div>
+      <div className='flex items-center'>
         <CartTotal />
+        <FaCartShopping size={30} />
         <ModeToggle />
       </div>
     </header>
