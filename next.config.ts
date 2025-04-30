ddd652e2fe8485bdfa6482bfb211a03c31a895ad
Craig Clayton <craig__clayton@outlook.com>
2025-04-30 22:21:47 +0100

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.dummyjson.com',
-        pathname: '/products/**',
-      },
-    ],
+    domains: ['cdn.dummyjson.com'],
   },
 };
 
