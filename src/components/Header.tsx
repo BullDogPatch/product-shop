0919d042025878e6ef1c3c5fad63a70708eb5ed1
Craig Clayton <craig__clayton@outlook.com>
2025-05-31 17:36:19 +0100

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
+import Link from 'next/link';
 import ShoppingCartModal from './cart-modal';
 import { ModeToggle } from './ModeToggle';
 
 const Header = () => {
   return (
     <header className='mb-2 h-24 p-4 flex justify-between items-center border-b-3 border-b-gray-800'>
-      <p>Shit-Shop</p>
+      <Link href='/'>Shit-Shop</Link>
       <div className='flex items-center'>
         <ShoppingCartModal />
         <ModeToggle />
